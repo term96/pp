051dae2edd7f15fa1b5158ee3f03fb0c37e34548
term96 <term96@yandex.com>
2018-05-14 17:24:28 +0300

    Добавлено чтение массива из файла

--- lw3/Lebedev_Andrey/2/BatcherSort/BatcherSort/BatcherSort.cpp
+++ lw3/Lebedev_Andrey/2/BatcherSort/BatcherSort/BatcherSort.cpp
@@ -4,10 +4,10 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
-#include <string>
+#include <fstream>
 #include "BatcherAlgorithm.h"
 
-void printVector(std::vector<int> const & vector)
+inline void printVector(std::vector<int> const & vector)
 {
 	for (size_t i = 0; i < vector.size(); i++)
 	{
@@ -16,7 +16,36 @@ void printVector(std::vector<int> const & vector)
 	std::cout << std::endl;
 }
 
-int main()
+int main(int argc, char * argv[])
 {
+	if (argc != 2)
+	{
+		std::cout << "Use: batchersort.exe <input file name>\n";
+		return 1;
+	}
+
+	std::ifstream file(argv[1]);
+	if (!file.is_open())
+	{
+		std::cout << "Failed to open file\n";
+		return 1;
+	}
+
+	std::vector<int> vector;
+	int num;
+	while (file >> num)
+	{
+		vector.push_back(num);
+	}
+
+	CBatcherAlgorithm batcherAlgorithm;
+	std::chrono::time_point<std::chrono::high_resolution_clock> startTime = std::chrono::high_resolution_clock::now();
+	batcherAlgorithm.sort(vector);
+	std::chrono::time_point<std::chrono::high_resolution_clock> endTime = std::chrono::high_resolution_clock::now();
+	long long timeElapsedInMs = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+
+	std::cout << "Algorithm work time: " << timeElapsedInMs << "ms\n" << "Result: ";
+	printVector(vector);
+
 	return 0;
 }
